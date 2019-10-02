63eda22ccbe004a9d8ee0a4a4a71822beea14ab7
DomiLP545 <52416135+DomiLP545@users.noreply.github.com>
2019-10-02 17:57:50 +0200

    Rename LED_mit_Button_Input.ino to 2_LED-input.ino

---
+++ 2_LED-input.ino
@@ -0,0 +1,29 @@
+/*
+Projekt 2 LED mit Button Input
+LED 11
+Button 2
+*/
+
+
+const int buttonPin = 2;     
+const int ledPin =  11;      
+
+
+int ButtonStatus = 0;        
+
+void setup() {
+ 
+  pinMode(ledPin, OUTPUT);
+  pinMode(buttonPin, INPUT);
+}
+
+void loop() {
+  ButtonStatus = digitalRead(buttonPin);
+
+ 
+  if ( ButtonStatus == HIGH) {
+    digitalWrite(ledPin, HIGH);
+  } else {
+    digitalWrite(ledPin, LOW);
+  }
+}
